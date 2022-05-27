5e99e402a4826e3a60e5ad0a8ec74a314d67971c
Elisha <83610362+ElishaFlacon@users.noreply.github.com>
2022-05-27 20:06:58 +0500

    upload first (alpha version) calculate
    первая версия калькулятора, пока что не умеет считать

---
+++ script.js
@@ -0,0 +1,147 @@
+
+
+// let appendFormInputSymbol = function (value) {
+//     formInput.value = value;
+// };
+
+
+
+
+//* ЗАДАЕМ КОНСТАНТЫ 
+
+let appendFormInputSymbol
+
+let MishaRak
+
+
+
+const formInput = document.querySelector('.form__input');
+const formInfo = document.getElementsByClassName('form__info');
+
+
+
+const allId =
+    [document.querySelector('#kC').addEventListener('click', appendFormInputSymbol = () => formInput.value = ''),
+    document.querySelector('#kC').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML = ""),
+    document.querySelector('#kSk').addEventListener('click', appendFormInputSymbol = () => formInput.value += '('),
+    document.querySelector('#kSk').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "О1"),
+    document.querySelector('#kSc').addEventListener('click', appendFormInputSymbol = () => formInput.value += ')'),
+    document.querySelector('#kSc').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОБКА"),
+    document.querySelector('#kSl').addEventListener('click', appendFormInputSymbol = () => formInput.value += '/'),
+    document.querySelector('#kSl').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОШ0КА"),
+    document.querySelector('#kx').addEventListener('click', appendFormInputSymbol = () => formInput.value += '*'),
+    document.querySelector('#kx').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОША"),
+    document.querySelector('#kM').addEventListener('click', appendFormInputSymbol = () => formInput.value += '-'),
+    document.querySelector('#kM').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОА"),
+    document.querySelector('#kP').addEventListener('click', appendFormInputSymbol = () => formInput.value += '+'),
+    document.querySelector('#kP').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОШ"),
+    document.querySelector('#kD').addEventListener('click', appendFormInputSymbol = () => formInput.value += '.'),
+    document.querySelector('#kD').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "А"),
+    document.querySelector('#kZ').addEventListener('click', appendFormInputSymbol = () => formInput.value += '%'),
+    document.querySelector('#kZ').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОШКА"),
+    document.querySelector('#kE').addEventListener('click', appendFormInputSymbol = () => formInput.value = 'ЖЕСТКО НАСЧИТАЛ'),
+    document.querySelector('#kE').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML = "НetОШИБКИ"),
+    document.querySelector('#k1').addEventListener('click', appendFormInputSymbol = () => formInput.value += '1'),
+    document.querySelector('#k1').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "32ИБКА"),
+    document.querySelector('#k2').addEventListener('click', appendFormInputSymbol = () => formInput.value += '2'),
+    document.querySelector('#k2').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОШd4А"),
+    document.querySelector('#k3').addEventListener('click', appendFormInputSymbol = () => formInput.value += '3'),
+    document.querySelector('#k3').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОШ2ИБКА"),
+    document.querySelector('#k4').addEventListener('click', appendFormInputSymbol = () => formInput.value += '4'),
+    document.querySelector('#k4').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОШИА"),
+    document.querySelector('#k5').addEventListener('click', appendFormInputSymbol = () => formInput.value += '5'),
+    document.querySelector('#k5').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОШ"),
+    document.querySelector('#k6').addEventListener('click', appendFormInputSymbol = () => formInput.value += '6'),
+    document.querySelector('#k6').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "Jib,rA"),
+    document.querySelector('#k7').addEventListener('click', appendFormInputSymbol = () => formInput.value += '7'),
+    document.querySelector('#k7').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОШИКА"),
+    document.querySelector('#k8').addEventListener('click', appendFormInputSymbol = () => formInput.value += '8'),
+    document.querySelector('#k8').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОКА"),
+    document.querySelector('#k9').addEventListener('click', appendFormInputSymbol = () => formInput.value += '9'),
+    document.querySelector('#k9').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ИШБКА"),
+    document.querySelector('#k0').addEventListener('click', appendFormInputSymbol = () => formInput.value += '0'),
+    document.querySelector('#k0').addEventListener('click', appendFormInputSymbol = () => formInfo[0].innerHTML += "ОШИБ9КА"),];
+
+// const allSimbol = {
+//     'C': true,
+//     '(': '(',
+//     ')': ')',
+//     '/': '/',
+//     'x': '*',
+//     '-': '-',
+//     '+': '+',
+//     ',': '.',
+//     '%': '%',
+//     '1': '1',
+//     '2': '2',
+//     '3': '3',
+//     '4': '4',
+//     '5': '5',
+//     '6': '6',
+//     '7': '7',
+//     '8': '8',
+//     '9': '9',
+//     '0': '0',
+// };
+
+allId.onclick = function asd() {
+
+};
+
+
+
+function TEST() {
+    console.log('TEST CONFIRM')
+};
+
+
+// checkClickOnButton(0);
+
+
+
+// function checkClickOnButton(btn) {
+//     allId[btn].onclick = function kkk() {
+//         TEST()
+//     };
+// };
+
+
+
+
+
+
+
+
+
+
+//! фУНКЦИИ
+
+// function appendFormInputSymbol() {
+
+//     for (let key in allSimbol) {
+//         if (allId === allSimbol[key]) {
+//             for (let value in allSimbol[key]) {
+//                 formInfo.innerHTML(value)
+//             };
+//         };
+//     };
+
+// };
+
+
+
+
+// console.log(formInput);
+// console.log(formInfo);
+// console.log(allId)
+
+
+
+
+
+
+
+
+
+
+
